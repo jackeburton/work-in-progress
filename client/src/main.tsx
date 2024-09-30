2ca5e2ee1ff50b6b591a55f9e69ace67bfc11fd9
Jack Burton <jackeburton97@gmail.com>
2024-09-30 12:51:58 +0100

    overhaul of reviewview

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './index.css'
+import './fonts.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 const queryClient = new QueryClient()
---
+++ dbscripts/populate_tables_longer_data.sql
@@ -0,0 +1,93 @@
+-- Insert fake users
+INSERT INTO users (username, email)
+VALUES 
+('JB', 'jb@fake.com');
+
+
+-- Insert fake submissions
+INSERT INTO submissions (userId, content)
+VALUES
+(1, 'The dim cold light of a cloudy morning enters the kitchen. He pours a scoop of coffee into the french press. The kettle clicks off as the boiling finishes. As the kettle''s rattle dissipates the cacophony of the dawn chorus fades back into the foreground of his hearing. Noticing the oncoming clouds out the window he chooses not to focus on them, instead searching for the family of birds in the trees that are making the dawn chorus so loud today. He fills the French press with the hot water in a circular fashion, covering all the grounds now so he doesn''t have to stir it with a spoon later. After it''s left to steep he divides the coffee evenly into two cups. She comes up and stands beside him.
+
+''Looks like it''s going to rain today.''
+''Typical. When do we get any kind of good weather on this godforsaken island? When do we ever get any sun at all here?'' His attempted focus switch from the rain in the day ahead to the noisy fauna of the garden clearly hadn''t been as successful as intended.
+''Don''t be such a debbie downer, we do, you just need to be patient. Besides, you know what they say - April showers bring May flowers.''
+He pauses while trying to search for a response. The thoughts of another month of pallid spring sun play over in his head. Strange amalgamations of GCSE Geography water cycle diagrams ring between his ears and the frustration, despite making complete emotional sense, no longer sits logically in his head. He takes a sip of coffee.
+
+''You sound like my Nan when you say that.'' He looks up from his coffee.
+''That''s because she was right, y''know. You should be grateful that the worst weather we get is clouds and rain. There are places in the world that don''t get any rain at all. When there''s no rain there''s no flowers. When there''s no flowers the view from that window would be even more depressing than it is now. Especially in summer.''
+''Why do I feel like you''re about to give me the ''there are children starving in Africa'' line.''
+''I''m just trying to say I feel a bit more grateful thinking that way. I''m sure some people would be gasping for rain.''
+''I get your point Lisa. I do. But I can''t mail my uneaten vegetables to South Sudan, they''ll go off in the postbox.''
+
+She moves away to continue her routine. He continues to peer out the window to the garden. All he hopes for is that the grey skies part all of a sudden and the sun streams onto his face. His body still pining from the lack of warmth from winter. Coffee still waiting patiently to be drunk. No words come to him. All he feels he needs is to be able to stare from the window onto broiling hot days. That ephemeral time of year where plans come easiest, excitement is cheap and the days will to be kept alive. The clouds weep for a second. They feel it too. They don''t long to be there any more than he is wishing them away. All seems to fade into yearning for something else.
+
+''I''m sorry, I think I''m just nervous.'' He says, breaking the silence.
+''It''s ok, I''d guessed so.''
+''It''s just a shitty garden centre job. I don''t know why I''m so worried.''
+''It''s important to you though?''
+''Yeah but it''s different than what I did before, there''s so much less pressure to perform, no-one breathing down my neck to take my job when I fuck up.''
+''Maybe it''s because you actually like working there?''
+''Yeah. I do actually enjoy it, I just can''t wrap my head around why I''m so nervous. There''s so much less riding on this. Like actively - it''s just selling plants and things for people''s gardens.''
+''What''s it been now, about 6 months?''
+''6 months to the day, that''s why I''m worrying about it - I think they''re going to go over my probation today.''
+''Has it really now? Wow, winter has come and gone so soon.''
+
+He searches for something to say. 
+
+For a small moment about 2 to 3 seconds after he''s done and no answer is found he just sits in silence. He is just sat at the table with her doing and thinking nothing. The sun will come one day. There is no worst case scenario. Everything changes. The job and probation review cease to exist. So long as he is absorbed in this moment with her, nothing else in the world matters. It all becomes so clear to him.
+
+He looks up at the clock. ''Fuck, is that the time? I need to leave.''
+'),
+(1, 'Truncated logs of satellite #2e4d5b from first mention of Jesse''.
+
+
+Pod day +93
+I keep staring out the bay window, trying to find the star you and I picked out those nights in the fields back home. The fires we used to have deep in the backcountry, thinking how dark it was outside of town. The stars seemed so clear. I didn''t know how bright they would be from up here, I wish you could see it too Jesse.
+
+Pod day +94
+Small debris shower last night tens of miles away, not close enough to affect the satellite''s system or instruments, everything running and operational. 
+
+Servers need rebooting though, new firmware update. Patch notes say they''re upping the network''s security protocol, making sure the data sent between satellites stays in order - might spend some time soon to learn from the source code how the whole thing actually works.
+
+
+Pod day +133
+Heavy debris shower - outer damage to satellite. I''ll be heading out later to make repairs. Significant issues but fixable, mainly the comms unit so the server has been stuck relaunching the research program. Pod atmosphere ecosystem stable, shower only affected the outer shell - epipremnum nursery branching new roots every day, oxygen reserves high, have enough for a while out there.
+
+Pod day +134
+Repairs went as planned. I took the chance to stay out longer than I needed, to watch it in more detail. Everything is continuing on the surface as nothing happened. The cogs still droning on and on. If I look closely I can tell the point where each city goes to sleep for the night, the lights switching off gradually and then all at once. I know I''m doing good work out here and contributing to the research but looking down on it all reminds me what I''m missing, how isolated we all are in the program. I remember that feeling though. The distance, when we used to stare up at the heavens. You and me Jesse that''s all it ever was. Knowing the sun some 800 thousand miles across but those tiny pinpricks of light out there being just like it - how alone it made us feel, how our mistakes never mattered on that scale.
+
+
+Pod day +167
+I''ve spent the last few weeks reading the source code for the comms protocol and I''ve come across something. It''s just morse code. Good old morse code. How strange is that? The network communication layer is kept up by dots and dashes, passed between the satellites that then gets converted into binary to be digested into digital formats that we process for the research. Satellites receive on one port of the comms unit and send back instrument data out to the network on another. The best part is - there''s unused ports. Probably in case any of them fail but if anyone else in the network has figured this out I need to see if I can send a message from one of the unused ports.
+Pod day +168
+Aside from reading source code, no changes in the past month. All systems operational, no debris showers and pod ecosystems still producing. Losing the will to check the server reports each morning. I keep staring at our star, hoping one day I''ll be able to touch your face again.
+
+
+Pod day +190
+I''ve programmed my own broadcaster on one of the ports. I turned it live last week and since then I''ve been monitoring the incoming messages, flushing out all the research data to see if anyone else in the network is doing the same. I''ve been sending my message every morning at the same time, signed with my satellite ID and a signifier to differentiate it from the research data. A simple - ''.. ...       .- -. -.-- --- -. .       .-.. .. ... - . -. .. -. --.'' / ''is anyone listening''. I keep waking up early each morning to check if there''s been anything back.
+
+
+Pod day +211
+Nothing so far, still checking the messages every day. Few showers last night, nothing meaningful to update on. Still hoping I wake up to something new in the data. Maybe one day I could call home this way, maybe you''ll still be there.
+
+Pod day + 212
+The firmware was patched today. Update reads: ''Comms protocol updated, messages only accepted from default port''. I stopped the messaging code this morning, no use in spending the energy anymore, no-one will even hear me now.
+
+
+Pod day +222
+I keep staring at our star, out the bay window again. First I thought it was my eyes from staring at the screens all day. It keeps turning off and back on again, a giant ball of gas blinking at me, the same pattern over and over : 
+''..       ... - .. .-.. .-..       .... . .- .-.       -.-- --- ..-'' / ''I still hear you''.
+
+
+Was that you Jesse?
+
+'),
+(1, 'It''s all the things
+I''d lose
+That went unnoticed
+
+Which voice corresponds to which number
+Annual cycles of green growth then stock brick
+The 5g tower built atop the new estate behind tesco
+');
